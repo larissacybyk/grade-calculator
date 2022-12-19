3975e6b0e231fbdf519a2eb011669fa92052d4da
larissacybyk <jdj9ng@virginia.edu>
2022-12-18 19:54:15 -0500

    basic layout of screen

--- src/main/java/gradecalc/CalcApplication.java
+++ src/main/java/gradecalc/CalcApplication.java
@@ -11,8 +11,7 @@ public class CalcApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalcApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 390, 600);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/gradecalc/CalcController.java
+++ src/main/java/gradecalc/CalcController.java
@@ -6,10 +6,15 @@ import javafx.scene.control.Label;
 public class CalcController {
     @FXML
     private Label welcomeText;
-
+    private String lime = "#76B947";
+    private String seafoam = "#2F5233";
+    private String kelly = "#B1D8B7";
+    private String spearmint = "#94C973";
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("How are your grades?");
-        welcomeText.setStyle("-fx-border-color: green; -fx-background-color: #eeeaea; -fx-text-fill: green; -fx-border-width: 3");
+    protected void onCalculateButtonClick() {
+
+    }
+    public void initialize(){
+        System.out.println("what");
     }
 }
